3c3e0593efb258882818141b994efcd7c31ab7e6
wangliu <wangliu023@qq.com>
2018-09-15 20:31:21 +0800

    sync

--- library/src/main/java/com/winnie/library/PullDownView.java
+++ library/src/main/java/com/winnie/library/PullDownView.java
@@ -1,9 +1,10 @@
 package com.winnie.library;
 
 import android.content.Context;
-import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
 
 public class PullDownView extends LinearLayout {
     public PullDownView(Context context) {
